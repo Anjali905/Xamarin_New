9d2ca6fa32878eb3cf944e3f1a595482abb2f9d9
Anjali Ajith <anjusukumari@gmail.com>
2021-04-16 09:17:09 +0530

    Method overriding with shapes

---
+++ Runtimepoly/Runtimepoly/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Runtimepoly
+{
+    class Shape
+    {
+        public virtual void Draw() { }
+    }
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle drawn");
+        }
+    }
+    class Triangle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Triangle drawn");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shape = new Shape[2];
+            shape[0] = new Rectangle();
+            shape[1] = new Triangle();
+            for(int i =0; i <= 1;i++)
+            {
+                shape[i].Draw();
+
+            }
+
+        }
+    }
+}
